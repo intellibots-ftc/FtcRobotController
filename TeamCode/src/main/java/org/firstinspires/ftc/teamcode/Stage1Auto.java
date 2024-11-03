d851bbee6001b9c95a0257c934158c36ff980e95
A-coder152 <156943520+A-coder152@users.noreply.github.com>
2024-11-03 00:35:05 -0400

    Update Stage1Auto.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Stage1Auto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Stage1Auto.java
@@ -5,6 +5,7 @@ import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
+import java.io.*;  
 import java.lang.Thread;
 
 /*
