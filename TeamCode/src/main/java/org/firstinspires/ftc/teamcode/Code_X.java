1de53e491169584b5a29ed349aec44d85bee5484
A-coder152 <156943520+A-coder152@users.noreply.github.com>
2024-11-03 00:29:24 -0400

    Update Code_X.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Code_X.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Code_X.java
@@ -36,6 +36,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 /*
