704debdde9449542d1c014a8361edcb6743246b5
A-coder152 <156943520+A-coder152@users.noreply.github.com>
2024-11-04 12:10:27 -0500

    Update Code_X.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Code_X.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Code_X.java
@@ -227,6 +227,7 @@ public class Code_X extends LinearOpMode {
 
             if (gamepad1.right_bumper && extensionMotor.getCurrentPosition() > -5750) {
                 extensionPower = Math.max(-1,(-5750-extensionMotor.getCurrentPosition())/200);
+                target = Math.max(target, (extensionMotor.getCurrentPosition()+500)/-10)
             } else if (gamepad1.left_bumper && extensionMotor.getCurrentPosition() < -50) {
                 extensionPower = Math.min(1,(-extensionMotor.getCurrentPosition())/200);
             } else {
