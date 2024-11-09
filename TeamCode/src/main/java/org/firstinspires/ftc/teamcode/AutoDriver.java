994c6bbfe803806f6814b47e14e7abe2b9359829
Wessam Bsata <111783555+wessam33@users.noreply.github.com>
2024-11-09 09:19:57 -0500

    Update AutoDriver.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoDriver.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoDriver.java
@@ -2,6 +2,8 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.CRServo;
 
 public class AutoDriver {
 
