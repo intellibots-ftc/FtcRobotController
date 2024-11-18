6852cc47fbb8a9ff6b27f53f2079cd50c41a4c8a
A-coder152 <156943520+A-coder152@users.noreply.github.com>
2024-11-18 12:38:34 -0500

    Create RobotControl.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotControl.java
@@ -0,0 +1,125 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.CRServo;
+
+public class RobotControl {
+
+    /* Declare OpMode members. */
+    private LinearOpMode myOpMode = null;   // gain access to methods in the calling OpMode.
+
+    // Define Motor and Servo objects  (Make them private so they can't be accessed externally)
+    private DcMotor leftFrontDrive = null;
+    private DcMotor leftBackDrive = null;
+    private DcMotor rightFrontDrive = null;
+    private DcMotor rightBackDrive = null;
+    private DcMotor armMotor = null;
+    private DcMotor extensionMotor=null;
+    private CRServo intakeServo =null;
+    private Servo intakeRotatorServo=null;
+
+    public static final double ARM_HANG_POS = -2000;
+    public static final double ARM_HIGH = -3500;
+    public static final double MAX_EXTENSION = -2050;
+
+    private double leftFrontPower  = 0;
+    private double rightFrontPower = 0;
+    private double leftBackPower   = 0;
+    private double rightBackPower  = 0;
+    private int armTarget = 0;
+    private int extTarget = 0;
+
+    // Define a constructor that allows the OpMode to pass a reference to itself.
+    public AutoDriver (LinearOpMode opmode) {
+        myOpMode = opmode;
+    }
+
+    /**
+     * Initialize all the robot's hardware.
+     * This method must be called ONCE when the OpMode is initialized.
+     * <p>
+     * All of the hardware devices are accessed via the hardware map, and initialized.
+     */
+    public void init()    {
+        // Define and Initialize Motors (note: need to use reference to actual OpMode).
+        leftFrontDrive  = myOpMode.hardwareMap.get(DcMotor.class, "left_front_drive");
+        leftBackDrive  = myOpMode.hardwareMap.get(DcMotor.class, "left_back_drive");
+        rightFrontDrive = myOpMode.hardwareMap.get(DcMotor.class, "right_front_drive");
+        rightBackDrive = myOpMode.hardwareMap.get(DcMotor.class, "right_back_drive");
+        armMotor   = myOpMode.hardwareMap.get(DcMotor.class, "arm_motor");
+        extensionMotor   = myOpMode.hardwareMap.get(DcMotor.class, "extension_motor");
+        intakeRotatorServo = myOpMode.hardwareMap.get(Servo.class, "intake_rotator_servo");
+        intakeServo = myOpMode.hardwareMap.get(CRServo.class, "intake_servo");
+
+        // To drive forward, most robots need the motor on one side to be reversed, because the axles point in opposite directions.
+        // Pushing the left stick forward MUST make robot go forward. So adjust these two lines based on your first test drive.
+        // Note: The settings here assume direct drive on left and right wheels.  Gear Reduction or 90 Deg drives may require direction flips
+        leftFrontDrive.setDirection(DcMotor.Direction.REVERSE);
+        rightFrontDrive.setDirection(DcMotor.Direction.FORWARD);
+        leftBackDrive.setDirection(DcMotor.Direction.REVERSE);
+        rightBackDrive.setDirection(DcMotor.Direction.FORWARD);
+
+        leftFrontDrive.setDirection(DcMotor.Direction.REVERSE);
+        leftBackDrive.setDirection(DcMotor.Direction.REVERSE);
+        rightFrontDrive.setDirection(DcMotor.Direction.FORWARD);
+        rightBackDrive.setDirection(DcMotor.Direction.FORWARD);
+        leftFrontDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightFrontDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        leftBackDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightBackDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        intakeServo.setDirection(DcMotor.Direction.REVERSE);
+        armMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        armMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        extensionMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        extensionMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        armMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        myOpMode.telemetry.addData(">", "Hardware Initialized");
+        myOpMode.telemetry.update();
+    }
+
+    public void controllerDrive(axial, lateral, yaw) {
+        leftFrontPower  = axial + lateral + yaw;
+        rightFrontPower = axial - lateral - yaw;
+        leftBackPower   = axial - lateral + yaw;
+        rightBackPower  = axial + lateral - yaw;
+
+        max = Math.max(Math.abs(leftFrontPower), Math.abs(rightFrontPower));
+        max = Math.max(max, Math.abs(leftBackPower));
+        max = Math.max(max, Math.abs(rightBackPower));
+
+        if (max > 1.0) {
+            leftFrontPower  /= max;
+            rightFrontPower /= max;
+            leftBackPower   /= max;
+            rightBackPower  /= max;
+        }
+    }
+
+    public int armControl(power, target){
+        if (power != 0){
+            int validity = 0;
+            if (power > 0 && armMotor.getCurrentPosition() < -50){
+                validity = 1;
+            } else if (power < 0 && armMotor.getCurrentPosition() > ARM_HIGH){
+                validity = 1;
+            }
+            armMotor.setPower(power * validity);
+            armMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            target = armMotor.getCurrentPosition();
+        } else {
+            armMotor.setTargetPosition(target);
+            armMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        }
+        return target;
+    }
+
+    public void update(mod){
+        leftFrontDrive.setPower(leftFrontPower * mod);
+        rightFrontDrive.setPower(rightFrontPower * mod);
+        leftBackDrive.setPower(leftBackPower * mod);
+        rightBackDrive.setPower(rightBackPower * mod);
+    }
+}
