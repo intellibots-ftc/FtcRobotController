8d8c1d4bea752102f8574324632128a7b40695cf
A-coder152 <156943520+A-coder152@users.noreply.github.com>
2024-11-30 00:40:39 -0500

    Create Stage3Auto.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Stage3Auto.java
@@ -0,0 +1,58 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+
+@Autonomous(name="Stage 3 Auto", group="Basic Auto")
+public class Stage3Auto extends LinearOpMode {
+    RobotControl robot = new RobotControl(this);
+    double final BASKET_X = 0;
+    double final BASKET_Y = 0;
+    double final SAMPLE_A_X = 0;
+    double final SAMPLE_A_Y = 0;
+    double final SAMPLE_B_X = 0;
+    double final SAMPLE_B_Y = 0;
+    double final SAMPLE_C_X = 0;
+    double final SAMPLE_B_Y = 0;
+    double final ARM_TAKE = 0;
+    double final EXTENSION_TAKE = 0;
+    
+
+    @Override
+    public void runOpMode(){
+        robot.init();
+        waitForStart();
+        robot.armTarget = robot.ARM_HIGH;
+        robot.extTarget = robot.MAX_EXTENSION;
+        robot.driveToTarget(BASKET_X, BASKET_Y, 45,0.2);
+        ElapsedTime timer = new ElapsedTime();
+        timer.reset();
+        robot.positionServo();
+        while (robot.controlOn == 0) {
+            // Do nothing, just wait
+        }
+        robot.powerDrive(0, 0, 0, 0);
+        robot.intake_servo.setPower(0.3);
+        timer.reset();
+        while (timer.seconds()<3){
+
+        }
+        eobot.driveToTarget(SAMPLE_A_X, SAMPLE_A_Y, 0, 0.2);
+        while (timer.seconds() < 4){
+
+        }
+        robot.armTarget = 1200;
+        timer.extTarget = EXTENSION_TAKE;
+        while(timer.seconds()<5){
+
+        }
+        robot.armTarget = ARM_TAKE;
+        while(timer.seconds()<8){
+
+        }
+    }
+}
