d3b88297b8320b0ae4803eec1faba421fa94d75e
A-coder152 <156943520+A-coder152@users.noreply.github.com>
2024-12-11 19:52:08 -0500

    Update EnhancedStage3Auto.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EnhancedStage3Auto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EnhancedStage3Auto.java
@@ -20,9 +20,9 @@ public class EnhancedStage3Auto extends LinearOpMode {
     private static final double BASKET_HEADING = -45;  // Degrees
     
     private static final double SAMPLE_A_X = 640;
-    private static final double SAMPLE_B_X = 300;
-    private static final double SAMPLE_C_X = 600;
-    private static final double SAMPLES_Y = -940;
+    private static final double SAMPLE_B_X = 860;
+    private static final double SAMPLE_C_X = 1150;
+    private static final double SAMPLES_Y = -950;
     private static final double SAMPLE_HEADING = 0;
     private static final double power = 0.2;
 
@@ -30,7 +30,7 @@ public class EnhancedStage3Auto extends LinearOpMode {
     private static final int ARM_SCORING = -2800;  // Scoring position
     private static final int ARM_COLLECTING = -0;  // Collecting position
     private static final int EXTENSION_SCORING = -2000;
-    private static final int EXTENSION_COLLECTING = -0;
+    private static final int EXTENSION_COLLECTING = -50;
 
     // Timeouts
     private static final double NAVIGATION_TIMEOUT = 7.0;  // seconds
