5b3b65f5a906ed3de564a46333262352adc6903f
A-coder152 <156943520+A-coder152@users.noreply.github.com>
2025-02-03 12:15:52 -0500

    Update FirstSpecimen.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FirstSpecimen.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FirstSpecimen.java
@@ -3,7 +3,7 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
-@Autonomous(group = "Specimen", name = "1 Spec Auto")
+@Autonomous(group = "Specimen", name = "5 Spec Auto")
 public class FirstSpecimen extends LinearOpMode {
     private AutoControl auto;
 
@@ -13,6 +13,8 @@ public class FirstSpecimen extends LinearOpMode {
         auto = new AutoControl(this);
         auto.initialize();
         auto.power = 0.8;
+        auto.weird = 1;
+        auto.wallWait = 0;
 
         while (!isStarted()) {
             auto.updateTelemetry();
@@ -21,12 +23,13 @@ public class FirstSpecimen extends LinearOpMode {
         // Main autonomous sequence
         // Move to scoring position
         auto.setup();
+        auto.robot.intakeServo.setPower(-1);
         auto.goToChamber(-400);
-        auto.wait(0.5);
+        auto.wait(0.6);
         auto.scoreSpecimen();
         auto.pushSpikeMark(-1500);
         auto.pushSpikeMark(-1720);
-        auto.grabFromWall();
+        auto.specimenFive();
         auto.goToChamber(-500);
         auto.skibidi = 2.5;
         auto.scoreSpecimen();
@@ -36,6 +39,10 @@ public class FirstSpecimen extends LinearOpMode {
         auto.grabFromWall();
         auto.goToChamber(-270);
         auto.scoreSpecimen();
+        auto.grabFromWall();
+        auto.goToChamber(-210);
+        auto.scoreSpecimen();
+        auto.moveTo(-1300, -310, 90, 1, 5, 1);
 
 
         // Park in designated area
